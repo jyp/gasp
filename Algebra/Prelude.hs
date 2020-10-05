7a7674f798733a4068a59a549a2e1fa0a75f4512
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2020-10-05 08:36:32 +0200

    draft prelude

---
+++ Algebra/Prelude.hs
@@ -0,0 +1,6 @@
+module Algebra.Prelude (module ReExport) where
+
+import Algebra.Classes as ReExport
+import Prelude as ReExport (Int,Integer,Float,Double, (==), Monoid(..), Ord(..), Real(..), Enum(..), snd, Rational, Functor(..), Eq(..), Bool(..), Semigroup(..), ($), Show(..), Char, String, uncurry, curry)
+import Control.Applicative as ReExport
+
