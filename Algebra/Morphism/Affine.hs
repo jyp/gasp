b737f2acc6ae2f8f9bc81da832978315141aa38a
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2022-01-28 15:00:27 +0100

    missing constraints

--- Algebra/Morphism/Affine.hs
+++ Algebra/Morphism/Affine.hs
@@ -24,10 +24,10 @@ data Affine x c = Affine c (LinComb x c)
 instance Multiplicative c => Scalable c (Affine x c) where
   k *^ x = k *< x
 
-instance (Ord x, AbelianAdditive c) => AbelianAdditive (Affine x c)
-instance (Ord x, Group c) => Group (Affine x c) where
+instance (Ord x, AbelianAdditive c,DecidableZero c) => AbelianAdditive (Affine x c)
+instance (Ord x, Group c,DecidableZero c) => Group (Affine x c) where
   negate = fmap negate
-instance (Ord x, Additive c) => Additive (Affine x c) where
+instance (Ord x, Additive c,DecidableZero c) => Additive (Affine x c) where
   (Affine c1 xs1) + (Affine c2 xs2) = Affine (c1 + c2) (xs1 + xs2)
   zero = Affine zero zero
 
@@ -36,13 +36,13 @@ splitVar x (Affine c0 (LinComb m)) = (M.findWithDefault zero x m, Affine c0 (Lin
 
 -- | @solve x f@ solves the equation @f == 0@ for x.
 -- Let f = k x + e.  If k == 0, return Left e. Otherwise, x and return Right -e/k. (The value of x)
-solve :: Ord scalar => (Eq scalar, Division scalar, Group scalar, Ord x)
+solve :: Ord scalar => (Eq scalar, Division scalar, Group scalar, Ord x,DecidableZero scalar)
       => x -> Affine x scalar -> Either (Affine x scalar) (Bool,Affine x scalar)
 solve x f = if k == zero then Left e else Right (k>zero,recip k *^ negate e) 
   where (k,e) = splitVar x f
 
 -- | Constant affine expression
-constant :: Additive c => Ord x => c -> Affine x c
+constant :: DecidableZero c => Ord x => c -> Affine x c
 constant c = Affine c zero
 
 var :: Multiplicative c => Additive c => v -> Affine v c
@@ -51,7 +51,7 @@ var x = Affine zero (LC.var x)
 eval :: forall x c v. (Additive x, Scalable x x) => (c -> x) -> (v -> x) -> Affine v c -> x
 eval fc fv (Affine c p) = fc c + LC.eval fc fv p
 
-subst :: (Ord x, Additive c, Multiplicative c) => (v -> Affine x c) -> Affine v c -> Affine x c
+subst :: (Ord x, DecidableZero c, Multiplicative c) => (v -> Affine x c) -> Affine v c -> Affine x c
 subst f (Affine c p) = constant c + LC.eval id f p 
 
 mapVars :: Ord x => (v -> x) -> Affine v c -> Affine x c
