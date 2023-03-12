319536599a7dac7d3c9c322f1a9432572a99dd94
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2023-03-12 20:28:34 +0100

    cats

--- Algebra/Category.hs
+++ Algebra/Category.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ConstrainedClassMethods #-}
 {-# LANGUAGE DefaultSignatures #-}
@@ -37,6 +39,11 @@ instance ProdObj Trivial where
   objfstsnd = Dict
   objunit = Dict
 
+instance SumObj Trivial where
+  objsum = Dict
+  objleftright = Dict
+  objzero = Dict
+
 
 type O2 k a b = (Obj k a, Obj k b)
 type O3 k a b c =
@@ -51,6 +58,9 @@ class Category (cat :: k -> k -> Type) where
   (.)      :: (Obj cat a, Obj cat b, Obj cat c) => b `cat` c -> a `cat` b -> a `cat` c
   id :: Obj cat a => a `cat` a
 
+class Category cat => Dagger cat where
+  dagger :: O2 cat a b => a `cat` b -> b `cat` a
+
 (∘) :: forall {k} (cat :: k -> k -> Type) a b c con. (Category cat, con ~ Obj cat, con a, con b, con c) => cat b c -> cat a b -> cat a c
 (∘) = (.) 
 
@@ -64,69 +74,126 @@ class ProdObj (con :: k -> Constraint) where -- TensorClosed constraint causes p
 class (ProdObj (Obj cat), Category cat) => Monoidal (cat :: k -> k -> Type) where
   (⊗)      :: (Obj cat a, Obj cat b, Obj cat c, Obj cat d) => (a `cat` b) -> (c `cat` d) -> (a ⊗ c) `cat` (b ⊗ d)
   assoc    :: (Obj cat a, Obj cat b, Obj cat c) => ((a ⊗ b) ⊗ c) `cat` (a ⊗ (b ⊗ c))
-  assoc'   :: (Obj cat a, Obj cat b, Obj cat c) => (a ⊗ (b ⊗ c)) `cat` ((a ⊗ b) ⊗ c)
+  assoc_   :: (Obj cat a, Obj cat b, Obj cat c) => (a ⊗ (b ⊗ c)) `cat` ((a ⊗ b) ⊗ c)
   unitorR   :: (Obj cat a) => a `cat` (a ⊗ One)
-  unitorR'  :: (Obj cat a) => (a ⊗ One) `cat` a
+  unitorR_  :: (Obj cat a) => (a ⊗ One) `cat` a
   unitorL   :: (Obj cat a, Obj cat One) => a `cat` (One ⊗ a)
-  unitorL'  :: (Obj cat a, Obj cat One) => (One ⊗ a) `cat` a
+  unitorL_  :: (Obj cat a, Obj cat One) => (One ⊗ a) `cat` a
 
-  default unitorL :: forall a. (SMC cat, Obj cat a) => a `cat` (One ⊗ a)
+  default unitorL :: forall a. (Symmetric cat, Obj cat a) => a `cat` (One ⊗ a)
   unitorL = swap ∘ unitorR
      \\ objprod @(Obj cat) @a @One
      \\ objprod @(Obj cat) @One @a
      \\ objunit @(Obj cat)
 
-  default unitorL' :: forall a. (SMC cat, Obj cat a) => (One ⊗ a) `cat` a 
-  unitorL' = unitorR' ∘ swap
+  default unitorL_ :: forall a. (Symmetric cat, Obj cat a) => (One ⊗ a) `cat` a 
+  unitorL_ = unitorR_ ∘ swap
      \\ objprod @(Obj cat) @a @One
      \\ objprod @(Obj cat) @One @a
      \\ objunit @(Obj cat)
 
 
-class Monoidal cat => SMC cat where
+class Monoidal cat => Symmetric cat where
   swap     :: (Obj cat a, Obj cat b) => (a ⊗ b) `cat` (b ⊗ a)
 
 class Monoidal k => Cartesian k where
-  exl   ::  {-<-} forall a b. O2 k a b                     => {->-}   (a ⊗ b) `k` a
-  exr   ::  {-<-} forall a b. O2 k a b                     => {->-}   (a ⊗ b) `k` b
-  dis   ::  {-<-} forall a.  Obj k a                      => {->-}   a `k` One
-  dup   ::  {-<-} forall a. Obj k a                   => {->-}   a `k` (a ⊗ a)
-  (▵)   ::  {-<-} forall a b c. (Obj k a,Obj k b, Obj k c) => {->-}   (a `k` b) -> (a `k` c) -> a `k` (b ⊗ c)
+  exl   ::   forall a b. O2 k a b                     =>    (a ⊗ b) `k` a
+  exr   ::   forall a b. O2 k a b                     =>    (a ⊗ b) `k` b
+  dis   ::   forall a.  Obj k a                      =>    a `k` One
+  dup   ::   forall a. Obj k a                   =>    a `k` (a ⊗ a)
+  (▵)   ::   forall a b c. (Obj k a,Obj k b, Obj k c) =>    (a `k` b) -> (a `k` c) -> a `k` (b ⊗ c)
 
   {-# MINIMAL exl,exr,dup | exl,exr,(▵) | dis,dup | dis,(▵) #-}
-  dis = disDefault
+  dis = exr . unitorR
+     \\ objprod @(Obj k) @a @One
+     \\ objunit @(Obj k)
   dup = id ▵ id
-  exl = exlDefault
-  exr = exrDefault
-  (▵) = (▵!)
+  exl = unitorR_ . (id ⊗ dis)
+          \\ objprod @(Obj k) @a @b
+          \\ objprod @(Obj k) @a @One
+          \\ objunit @(Obj k)
+  exr = unitorL_ ∘ (dis ⊗ id)
+          \\ objprod @(Obj k) @a @b
+          \\ objprod @(Obj k) @One @b
+          \\ objunit @(Obj k)
+  f ▵ g = (f ⊗ g) ∘ dup 
+          \\ objprod @(Obj k) @a @a
+          \\ objprod @(Obj k) @b @c 
+
+cartesianCross :: (Obj k (b1 ⊗ b2), Obj k b3, Obj k c, Obj k b1,
+                     Obj k b2, Cartesian k) =>
+                    k b1 b3 -> k b2 c -> k (b1 ⊗ b2) (b3 ⊗ c)
+cartesianCross a b = (a . exl) ▵ (b . exr)
+
 
 type TensorCon con = forall a b. (con a, con b) => con (a⊗b) :: Constraint
 type LConTensor con = forall a b. con (a⊗b) => con a :: Constraint
 type RConTensor con = forall a b. con (a⊗b) => con a :: Constraint
 
+type SumObj :: forall {k}. (k -> Constraint) -> Constraint
+class SumObj (con :: k -> Constraint) where -- TensorClosed constraint causes problems in the Free module. (probabjy GHC bug)
+  objsum :: (con a, con b) => Dict (con (a⊕b))
+  objleftright :: forall z a b. (z ~ (a⊕b), con z) => Dict (con a, con b)
+  objzero :: Dict (con Zero)
 
-disDefault :: forall k a. (Cartesian k, Obj k a ) =>  a `k` One
-disDefault = exr . unitorR
-     \\ objprod @(Obj k) @a @One
-     \\ objunit @(Obj k)
+class (SumObj (Obj cat), Category cat) => Monoidal' (cat :: k -> k -> Type) where
+  (⊕)      :: (Obj cat a, Obj cat b, Obj cat c, Obj cat d) => (a `cat` b) -> (c `cat` d) -> (a ⊕ c) `cat` (b ⊕ d)
+  assoc'    :: (Obj cat a, Obj cat b, Obj cat c) => ((a ⊕ b) ⊕ c) `cat` (a ⊕ (b ⊕ c))
+  assoc_'   :: (Obj cat a, Obj cat b, Obj cat c) => (a ⊕ (b ⊕ c)) `cat` ((a ⊕ b) ⊕ c)
+  unitorR'   :: (Obj cat a) => a `cat` (a ⊕ Zero)
+  unitorR_'  :: (Obj cat a) => (a ⊕ Zero) `cat` a
+  unitorL'   :: (Obj cat a, Obj cat Zero) => a `cat` (Zero ⊕ a)
+  unitorL_'  :: (Obj cat a, Obj cat Zero) => (Zero ⊕ a) `cat` a
 
-exlDefault :: forall k con a b. (con ~ Obj k, Cartesian k, O2 k a b) =>  (a ⊗ b) `k` a
-exlDefault = unitorR' . (id ⊗ dis)
-          \\ objprod @(Obj k) @a @b
-          \\ objprod @(Obj k) @a @One
-          \\ objunit @(Obj k)
+  default unitorL' :: forall a. (Symmetric' cat, Obj cat a) => a `cat` (Zero ⊕ a)
+  unitorL' = swap' ∘ unitorR'
+     \\ objsum @(Obj cat) @a @Zero
+     \\ objsum @(Obj cat) @Zero @a
+     \\ objzero @(Obj cat)
 
-exrDefault :: forall k con a b. (con ~ Obj k,Cartesian k, O2 k a b) =>  (a ⊗ b) `k` b
-exrDefault = unitorL' ∘ (dis ⊗ id)
-          \\ objprod @(Obj k) @a @b
-          \\ objprod @(Obj k) @One @b
-          \\ objunit @(Obj k)
+  default unitorL_' :: forall a. (Symmetric' cat, Obj cat a) => (Zero ⊕ a) `cat` a 
+  unitorL_' = unitorR_' ∘ swap' 
+     \\ objsum @(Obj cat) @a @Zero
+     \\ objsum @(Obj cat) @Zero @a
+     \\ objzero @(Obj cat)
 
 
-(▵!) :: forall k con a b c . (con ~ Obj k,Cartesian k,O3 k a b c) =>   (a `k` b) -> (a `k` c) -> a `k` (b ⊗ c)
-f ▵! g = (f ⊗ g) ∘ dup 
-          \\ objprod @(Obj k) @a @a
-          \\ objprod @(Obj k) @b @c 
+
+class Monoidal' cat => Symmetric' cat where
+  swap'     :: (Obj cat a, Obj cat b) => (a ⊕ b) `cat` (b ⊕ a)
+
+type Cartesian' :: forall {k}. (k -> k -> Type) -> Constraint
+class Monoidal' cat => Cartesian' cat where
+  exl'  ::   forall a b. O2 cat a b                     =>    (a ⊕ b) `cat` a
+  exr'  ::   forall a b. O2 cat a b                     =>    (a ⊕ b) `cat` b
+  dis'  ::   forall a.   Obj cat a                      =>    a `cat` Zero
+  dup'  ::   (Obj cat a, Obj cat (a⊕a))                   =>    a `cat` (a ⊕ a)
+  (▴)   ::   forall a b c. (Obj cat a,Obj cat b, Obj cat c) =>    (a `cat` b) -> (a `cat` c) -> a `cat` (b ⊕ c)
+
+  {-# MINIMAL exl',exr',dup' | exl',exr',(▴) | dis',dup' | dis',(▴) #-}
+  dis' = exr' . unitorR'
+     \\ objsum @(Obj cat) @a @Zero
+     \\ objzero @(Obj cat)
+  dup' = id ▴ id
+  exl' = unitorR_' . (id ⊕ dis')
+          \\ objsum @(Obj cat) @a @b
+          \\ objsum @(Obj cat) @a @Zero
+          \\ objzero @(Obj cat)
+  exr' = unitorL_' ∘ (dis' ⊕ id)
+          \\ objsum @(Obj cat) @a @b
+          \\ objsum @(Obj cat) @Zero @b
+          \\ objzero @(Obj cat)
+  f ▴ g = (f ⊕ g) ∘ dup' 
+          \\ objsum @(Obj cat) @a @a
+          \\ objsum @(Obj cat) @b @c 
+
+
+class Monoidal' k => CoCartesian' k where
+  inl'   ::  O2 k a b                                 =>  a `k` (a ⊕ b)
+  inr'   ::  O2 k a b                                 =>  b `k` (a ⊕ b)
+  new'   ::  forall a. (Obj k a)                      =>  Zero `k` a
+  jam'   ::  Obj k a                                  =>  (a ⊕ a) `k` a
+  (▾)    ::  forall a b c. (Obj k a,Obj k b, Obj k c) =>  (b `k` a) -> (c `k` a) -> (b ⊕ c) `k` a
 
 
 
@@ -140,4 +207,34 @@ instance Category (->) where
   (.) = (Prelude..)
   id = Prelude.id
 
+instance Monoidal (->) where
+  (f ⊗ g) (x `Pair` y) = (f x `Pair` g y)
+  assoc ((x `Pair` y) `Pair` z) = (x `Pair` (y `Pair` z)) 
+  assoc_ (x `Pair` (y `Pair` z)) = ((x `Pair` y) `Pair` z)  
+  unitorR x = (x `Pair` Unit)
+  unitorR_ (x `Pair` Unit) = x
 
+instance Symmetric (->) where
+  swap (x `Pair` y) = (y `Pair` x)
+
+instance Monoidal' (->) where
+  f ⊕ g = \case
+    Inj1 x -> Inj1 (f x)
+    Inj2 x -> Inj2 (g x)
+  assoc' = \case
+    Inj1 (Inj1 x) -> Inj1 x
+    Inj1 (Inj2 x) -> Inj2 (Inj1 x)
+    Inj2 x -> Inj2 (Inj2 x)
+  assoc_' = \case
+    (Inj1 x) -> (Inj1 (Inj1 x)) 
+    (Inj2 (Inj1 x)) -> (Inj1 (Inj2 x)) 
+    (Inj2 (Inj2 x)) -> (Inj2 x) 
+  unitorR' x = Inj1 x
+  unitorR_' = \case
+    Inj1 x -> x
+    Inj2 x -> case x of
+
+instance Symmetric' (->) where
+  swap' = \case
+    Inj1 x -> Inj2 x
+    Inj2 x -> Inj1 x
--- Algebra/Types.hs
+++ Algebra/Types.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE EmptyDataDeriving #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -19,26 +25,73 @@ class AlgebraicKind k where
 
 
 instance AlgebraicKind Type where
-  data x ⊕ y = Inj1 x | Inj2 y deriving (Eq,Ord)
-  data x ⊗ y = Pair {π1 :: x, π2 :: y} deriving (Eq,Ord)
-  data Dual x = DualType x deriving (Eq,Ord)
-  data One = Unit deriving (Eq,Ord)
-  data Zero deriving (Eq,Ord)
+  data x ⊕ y = Inj1 x | Inj2 y deriving (Eq,Ord,Show)
+  data x ⊗ y = Pair {π1 :: x, π2 :: y} deriving (Eq,Ord,Show)
+  data Dual x = DualType x deriving (Eq,Ord,Show)
+  data One = Unit deriving (Eq,Ord,Enum,Bounded,Show)
+  data Zero deriving (Eq,Ord,Show)
 
+
+
+inhabitants :: Finite a => [a]
+inhabitants = [minBound..maxBound]
+
+class (Enum a, Bounded a) => Finite a where
+  typeSize :: Int
+  typeSize = fromEnum (maxBound @a) - fromEnum (minBound @a) + 1
+
+fromZero :: forall a. Finite a => Int -> a
+fromZero i = toEnum (i + fromEnum (minBound @a))
+
+instance (Bounded x, Bounded y) => Bounded (x⊕y) where
+  minBound = Inj1 minBound
+  maxBound = Inj2 maxBound
+
+instance (Finite x, Finite y) => Enum (x⊕y) where
+  toEnum i = if i < typeSize @x then Inj1 (toEnum i) else Inj2 (toEnum (i-typeSize @x))
+  fromEnum = \case
+     Inj1 x -> fromEnum x
+     Inj2 x -> fromEnum x + typeSize @x
+
+instance (Finite x, Finite y) => Finite (x⊕y) where
+instance (Finite x, Finite y) => Enum (x⊗y) where
+  toEnum k = Pair (toEnum i) (toEnum j)
+    where (j,i) = k `divMod` typeSize @x
+  fromEnum (Pair x y) = fromEnum x + fromEnum y * (typeSize @x)
+instance (Finite x, Finite y) => Finite (x⊗y) where
+instance Finite Bool
+instance Finite One
+
+instance (Bounded x, Bounded y) => Bounded (x⊗y) where
+  minBound = minBound `Pair` minBound
+  maxBound = maxBound `Pair` maxBound
+
+  
+instance Enum Zero where
+  toEnum = error "toEnum: Zero"
+  fromEnum = \case
+instance Bounded Zero where
+  minBound = error "minBound: Zero"
+  maxBound = error "maxBound: Zero"
+instance Finite Zero where
+  typeSize = 0
 
 -- | Algebraic structure for (Type -> Type) is in the exponential
--- level because functor composition is generally where the action is.
+-- level Finitecause functor composition is generally where the action is.
 instance AlgebraicKind (Type -> Type) where
   data (f ⊗ g) x = Comp {fromComp :: (f (g x))} deriving (Foldable, Traversable, Functor)
   data (f ⊕ g) x = FunctorProd (f x) (g x) deriving (Foldable, Traversable, Functor)
   data One x = FunctorUnit x deriving (Foldable, Traversable, Functor)
   data Dual f x = FunctorDual {fromFunctorDual :: f x} deriving (Foldable, Traversable, Functor)
-  data Zero x = Const {fromConst :: x} deriving (Foldable, Traversable, Functor)
+  data Zero x = FunctorZero deriving (Foldable, Traversable, Functor)
 
 instance Applicative One where
   pure = FunctorUnit
   FunctorUnit f <*> FunctorUnit x = FunctorUnit (f x)
   
+instance Applicative Zero where
+  pure _ = FunctorZero
+  _ <*> _ = FunctorZero
 
 instance (Applicative f, Applicative g) => Applicative (f ⊗ g) where
   Comp f <*> Comp x = Comp ((fmap (<*>) f) <*> x)
