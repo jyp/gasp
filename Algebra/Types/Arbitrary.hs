70494d012fedf7b131048cb10db8af573bdbb531
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2023-03-13 20:25:41 +0100

    arb

--- Algebra/Types/Arbitrary.hs
+++ Algebra/Types/Arbitrary.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE PolyKinds #-}
@@ -18,6 +19,7 @@ data Some1 f where
 
 data Repr :: k -> Type where
   RPlus :: Repr a -> Repr b -> Repr (a ⊕ b)
+  RTimes :: Repr a -> Repr b -> Repr (a ⊗ b)
   ROne :: Repr One
   RZero :: Repr Zero
 
@@ -26,16 +28,23 @@ data Repr :: k -> Type where
 sizedArbRepr :: Int -> Gen (Some1 Repr)
 sizedArbRepr n
   | n <= 1 = frequency [(1,pure(Some1 RZero)), (3,pure(Some1 ROne))]
-  | otherwise = 
-  
--- sizedArbRepr n = do
---   es <- vectorOf 2 (sizedArbRepr (n-1))
---   elements [R es, RTimes es]
+  | otherwise = do
+      Some1 l <- sizedArbRepr (n `div` 2)
+      Some1 r <- sizedArbRepr (n `div` 2)
+      elements [Some1 (RPlus l r),Some1 (RTimes l r)]
   
 instance Arbitrary (Some1 Repr) where
-  arbitrary = _
+  arbitrary = sized sizedArbRepr
 
-arbitrary1 :: Gen (Some1 f) -> (forall x. f x -> prop) -> Gen prop
-arbitrary1 g f = do
-  Some1 a <- g
-  return (f a)
+arbitraryF1 :: Gen (Some1 repr) -> (forall x. repr x -> Gen (f x)) -> (forall x. f x -> prop) -> Gen prop
+arbitraryF1 r g p = do
+  Some1 a <- r
+  b <- g a
+  return (p b)
+  
+arbitraryF2 :: Gen (Some1 repr) -> Gen (Some1 repr) -> (forall x y. repr x -> repr y -> Gen (f x y)) -> (forall x y. f x y -> prop) -> Gen prop
+arbitraryF2 r1 r2 g p = do
+  Some1 a <- r1
+  Some1 a' <- r2
+  b <- g a a'
+  return (p b)
